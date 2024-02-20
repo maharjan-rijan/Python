70dc07a183cf7ccbd67bc3844e5566270c338fb9
Rijan Maharjan <rijanq0@gmail.com>
2024-02-20 11:57:38 +0545

    New file added

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello World")
+first_name = "Rijan"
+last_name = "Maharjan"
+# name = type(first_name)
+print(type(first_name), type(last_name))
+print(first_name, last_name, sep= " ")
+print(first_name+" "+last_name)
+print()
---
+++ var.py
@@ -0,0 +1,6 @@
+a = float(4)
+b = 2.4
+c = "Apple"
+print(type(a))
+print(type(b))
+print(type(c))
