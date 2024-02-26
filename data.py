401b3bfd083d31dc3e1284a8dcb7180eceb4a67d
Rijan Maharjan <rijanq0@gmail.com>
2024-02-26 11:13:35 +0545

    Some Change

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # Data types => Numberic datatype, Sqquence datatype, Bollen datatype, Mapping datatype, Binary datatype,
                 #none datatype, set datatype
-num_1 = 5
-num_2 = 7
+num_1 = complex(4,5)
+num_2 = complex(3,7)
 
 Sum = num_1 +num_2
 Multiplication = num_1 * num_2
@@ -11,3 +11,5 @@ print("Addition", Sum, sep=" = ")
 print(f"Subtraction {Subtraction}")
 print("Multiplication", Multiplication)
 print("Division", Division)
+print(num_1.real)
+print(num_2.imag)
