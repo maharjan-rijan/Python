cb9d7013254d9a8268a419650e863c023297f4c5
Rijan Maharjan <rijanq0@gmail.com>
2024-02-27 12:14:51 +0545

    Python list and tuple

---
+++ tuple_list.py
@@ -0,0 +1,6 @@
+import sys
+fruit_list = ["Apple","Banana","Orange"]
+fruit_tuple = ("Apple","Banana","Orange")
+
+print("List Memory Size",sys.getsizeof(fruit_list))
+print("List Memory Size",sys.getsizeof(fruit_tuple))
