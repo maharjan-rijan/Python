edee722e87b7003fd9806bc2c0c71ab0bd198508
Rijan Maharjan <rijanq0@gmail.com>
2024-03-04 12:25:00 +0545

    Modification

--- Assignment4.py
+++ Assignment4.py
@@ -1,5 +1,5 @@
-#If customer by a product and if total price is greater than 1000, than give10% discount other. Print the
-#discount value and the total amount to be paid.
+#If customer by a product and if total price is greater than 1000, than give 10% discount otherwise 
+# give 5% discount. Print the discount value and the total amount to be paid.
 amount = int(input('Enter the amount in number = '))
 discount = 0
 
