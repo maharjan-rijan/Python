046e6541345099663f495dc3914b837a1dcce43b
Rijan Maharjan <rijanq0@gmail.com>
2024-03-10 12:20:02 +0545

    Python Day 10

---
+++ Day_10/loop.py
@@ -0,0 +1,12 @@
+# Loop -> For Loop, While Loop
+# countries = ["Afghanistan", "Bangladesh", "Bhutan", "India", "Maldives", "Nepal", "Pakistan","Sri Lanka"]
+# print("The name of SAARC Countries:")
+# for country in countries:
+#     print("The countries is",country)
+
+# While Loop
+i = 2
+
+while i<= 10:
+    print(i)
+    i = i+1
