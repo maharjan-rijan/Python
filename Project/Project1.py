74e44fba8f83267c2b03dd9f5b2ffe02cfc88192
Rijan Maharjan <rijanq0@gmail.com>
2024-03-12 11:03:29 +0545

    Some Modification

--- Project/Project1.py
+++ Project/Project1.py
@@ -3,17 +3,38 @@
 # 1. Add task
 # 2. View task
 # 3. Remove task
-Choose = int(input("Enter your choice:"))
 
-match Choose:
-    case 1:
-        print("Add task")
-    case 2:
-        print("View task")
-    case 3:
-        print("Remove Task")
-    case _:
-        print("Exist")
-while Choose == 1:
-    print("Enter a tsak ")
-    break
+todolist=[]
+while True:
+    print("TODO list:")
+    print("Enter 1 to add list: ")
+    print("Enter 2 to complete added list: ")
+    print("Enter 3 to display list: ")
+    print("Enter 4 to remove list: ")
+    print("Enter 5 to exit list: ")
+    choice=int(input("enter choice: "))
+    if choice==1:
+        x = input("Enter task: ")
+        todolist.append(x)
+        print(f"{x} successfully added")
+    elif choice == 2:
+        x = input("Enter task to complete: ")
+        if x in todolist:
+            todolist.remove(x)
+            print(f"{x} successfully completed")
+        else:
+            print(f"{x} task not found")
+    elif choice==3:
+            print(todolist[x])
+    elif choice==4:
+        x = input("Enter task to complete: ")
+        if x in todolist:
+            todolist.remove(x)
+            print(f"{x} successfully removed")
+        else:
+            print(f"{x} not found to remove")
+    elif choice==5:
+        print("list completed ready to exist: ")
+        break
+    else:
+        print("Invalid choice")
