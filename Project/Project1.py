f422de0d5255576fc4d5d81650b46cf18b24b7da
Rijan Maharjan <rijanq0@gmail.com>
2024-03-12 11:39:41 +0545

    Some Modification

--- Project/Project1.py
+++ Project/Project1.py
@@ -1,40 +1,28 @@
 #create a CLT TO DO application. User should able to add task, view task, mark the task as completed and remove the task 
 #Choose task: 
 # 1. Add task
-# 2. View task
-# 3. Remove task
-
-todolist=[]
+# 2. Make as Completed
+# 3. View task
+# 4. Remove task
+# 5. Exist
+#========================================================================================================================#
+#initialize empty todos to store todo items
+toDoList = [] 
 while True:
-    print("TODO list:")
-    print("Enter 1 to add list: ")
-    print("Enter 2 to complete added list: ")
-    print("Enter 3 to display list: ")
-    print("Enter 4 to remove list: ")
-    print("Enter 5 to exit list: ")
-    choice=int(input("enter choice: "))
-    if choice==1:
-        x = input("Enter task: ")
-        todolist.append(x)
-        print(f"{x} successfully added")
-    elif choice == 2:
-        x = input("Enter task to complete: ")
-        if x in todolist:
-            todolist.remove(x)
-            print(f"{x} successfully completed")
-        else:
-            print(f"{x} task not found")
-    elif choice==3:
-            print(todolist[x])
-    elif choice==4:
-        x = input("Enter task to complete: ")
-        if x in todolist:
-            todolist.remove(x)
-            print(f"{x} successfully removed")
-        else:
-            print(f"{x} not found to remove")
-    elif choice==5:
-        print("list completed ready to exist: ")
-        break
-    else:
-        print("Invalid choice")
+    print("\nToDo List Manager:")
+    print("1. Add Task")
+    print("2. Mark as completed")
+    print("3. View Tasks")
+    print("4. Remove Task")
+    print("5. Exist\n")
+    
+    choice = input("Enter your choice (1-5):")
+    
+    if choice == '1':
+        task_name = input("Enter the task name:")
+        task = {
+            'task_name' : task_name,
+            'is_completed' : False,
+        }
+        toDoList.append(task)
+        print("Task Added Succesfully")
