208756a6b6ad1b1b48b0d863a5a610737ecd7240
Rijan Maharjan <rijanq0@gmail.com>
2024-03-25 20:53:26 +0545

    Python assignment complete

--- Assignment/Assignment16.py
+++ Assignment/Assignment16.py
@@ -1,4 +1,22 @@
 # Write a python program that ask two number and a arthimatic operation 
 # (+, -, *, /) from user can on the basic of the arithmetic operation provided by the 
-# user, calculate its value. To calculate and print its value,c`reate function for 
+# user, calculate its value. To calculate and print its value,create function for 
 # each operations. Use keyword argument to call the function.
+
+def numberCalculate(num1, num2)->None:
+    choice = input("Enter any Operation:")
+    if choice == "+":
+        sum = num1 + num2
+        print(f"The sum is {sum}")
+    elif choice == "-":
+        difference = num1 - num2
+        print(f"The difference is {difference}")
+    elif choice == "*":
+        multiplication = num1 * num2
+        print(f"The multiplication is {multiplication}")
+    elif choice == "/":
+        division = num1 / num2
+        print(f"The division is {division}")
+    else:
+        print("Arithmetic Operation Cannot performe.")
+numberCalculate(num1 = 4, num2 = 6)
